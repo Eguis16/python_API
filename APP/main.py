1a5260b0497ff181854497c44a6e28006f9edb2c
Esuarez16 <eguis.suarez16@gmail.com>
2025-01-07 14:11:20 -0300

    actualizacion 06-01-2025

--- APP/main.py
+++ APP/main.py
@@ -21,7 +21,11 @@ app.config.from_mapping(
 @app.route('/')
 def home():
     return render_template("index.html")
-    
+
+
+@app.route('/inicio')
+def inicio():
+    return render_template('inicio.html')
 if __name__ == '__main__':
     app(debug=True)
 
